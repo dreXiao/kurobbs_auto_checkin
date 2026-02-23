8f70ee86613d0605a4a9850193c4b58bc03e92df
dreXiao <92026485+dreXiao@users.noreply.github.com>
2026-02-23 16:58:42 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -38,6 +38,7 @@ class Settings:
             qmsg_token=os.getenv("QMSG_TOKEN"),
             qq=os.getenv("QQ"),
             bot=os.getenv("BOT"),
+            qmsg_server_url=f"https://qmsg.zendee.cn/send/{qmsg_token}"
         )
 
     def sensitive_values(self) -> List[str]:
