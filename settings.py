240d7475311c42cf9655cac7a077fee17157c8c6
dreXiao <92026485+dreXiao@users.noreply.github.com>
2026-05-09 12:38:06 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -44,7 +44,7 @@ class Settings:
             bot=os.getenv("BOT"),
             group_id=os.getenv("GROUP_ID"),
             qmsg_server_url="https://qmsg.zendee.cn/jsend",
-            napcat_server_url="https://api.napcat.noyoru.top/send_msg"
+            napcat_server_url="http://139.129.25.129:3000/send_msg"
         )
 
     def sensitive_values(self) -> List[str]:
